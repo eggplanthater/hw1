2878979565caae65f8ccfc8017068c9bbdd5d726
Brian <eggpanthater@gmail.com>
2014-10-04 01:16:20 -0700

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   #get '/about', to: 'CONTROLLER#FUNCTION'
   #get '/user/new', to: 'CONTROLLER#FUNCTION', as: 'users'
 
-
-
+  root 'users#index'
+  get '/user/new', to: 'users#new', as: 'users'
+  get '/about', to: 'static_pages#about'
   #Edit above this line
   get '/user/:id', to: 'users#show', as: 'user'
   post '/user/new', to: 'users#create'
